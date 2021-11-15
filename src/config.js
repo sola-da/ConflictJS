9367639b95560436944d80523b4c5080ad910366
Yu Mabuchi <60955318+new-marty@users.noreply.github.com>
2021-11-15 15:53:22 +0900

    initial setup

--- src/config.js
+++ src/config.js
@@ -1,36 +1,40 @@
 // Author: Michael Pradel, Jibesh Patra
 
-(function () {
-    const pathExists = require('./utilities/pathExists').pathExists;
-    const fs = require('fs');
-    exports.nbGeneratedTests = 50; // Number of tests to generate
-    exports.callsPerGeneratedTest = 10; // Number of times to call the function
-    exports.serverPort = 3000;
+;(function () {
+  const pathExists = require('./utilities/pathExists').pathExists
+  const fs = require('fs')
+  exports.nbGeneratedTests = 50 // Number of tests to generate
+  exports.callsPerGeneratedTest = 10 // Number of times to call the function
+  exports.serverPort = 3000
 
-    exports.waitingTime = 30000; // Time to wait before killing the experiments
-    exports.timeout_kill_job = 120000; // If a job runs more than set time kill it (60000 = 1 minute)
-    exports.browserName = 'chromium-browser';
+  exports.waitingTime = 30000 // Time to wait before killing the experiment
+  exports.timeout_kill_job = 120000 // If a job runs more than set time kill it (60000 = 1 minute)
+  exports.browserName = 'chromium-browser'
+  exports.browserPath = '/Applications/Chromium.app/Contents/MacOS/Chromium'
 
-// The result file
-    let currentDir = process.cwd();
-    let resultDir = "results";
+  // The result file
+  let currentDir = process.cwd()
+  let resultDir = 'results'
 
-    if (!pathExists(currentDir + '/' + resultDir)) fs.mkdirSync(currentDir + '/' + resultDir);
-    exports.validatedProblematicLibrariesFile = currentDir + '/' + resultDir + '/validated-conflicts.json';
-    exports.singleLibCrashingFile = currentDir + '/' + resultDir + '/filtered-InclusionCrash.json';
+  if (!pathExists(currentDir + '/' + resultDir))
+    fs.mkdirSync(currentDir + '/' + resultDir)
+  exports.validatedProblematicLibrariesFile =
+    currentDir + '/' + resultDir + '/validated-conflicts.json'
+  exports.singleLibCrashingFile =
+    currentDir + '/' + resultDir + '/filtered-InclusionCrash.json'
 
-    /**
-     * Test these characters for uniqueness in src/utilities/findUniqueChars.js
-     * @type {string}
-     */
-    exports.libraryNamesConjunctionChar = '§';
-    exports.accessPathSeparationChar = '°';
-    exports.fileNamePrefixSeparationChar = 'ß';
+  /**
+   * Test these characters for uniqueness in src/utilities/findUniqueChars.js
+   * @type {string}
+   */
+  exports.libraryNamesConjunctionChar = '§'
+  exports.accessPathSeparationChar = '°'
+  exports.fileNamePrefixSeparationChar = 'ß'
 
-// Need the following directories in order to function
-    exports.resultsDirectory = resultDir;
-    exports.generationDirectory = "/generated"; // A directory where all generated tests and HTML files are kept
-    exports.benchmarkDir = "/benchmarks"; // Contains libraryNames we want to test
-    exports.htmlFragmentsDir = "src/htmlFragments"; // Contains libraryNames we want to test
-    exports.globalWritesDir = "/global-writes/";
-})();
+  // Need the following directories in order to function
+  exports.resultsDirectory = resultDir
+  exports.generationDirectory = '/generated' // A directory where all generated tests and HTML files are kept
+  exports.benchmarkDir = '/benchmarks_all' // Contains libraryNames we want to test
+  exports.htmlFragmentsDir = 'src/htmlFragments' // Contains libraryNames we want to test
+  exports.globalWritesDir = '/global-writes/'
+})()
